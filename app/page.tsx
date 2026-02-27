095b63b639cb7b36469f22a8ed46647e8b74e8e3
Krzysztof Jurkowski <krzysztof.jurkowski@tm1.edu.pl>
2026-02-27 15:44:15 +0100

    IDK why it should work

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import { ComponentExample } from "@/components/component-example";
+import { db } from "@/lib/db";
+import { collection, doc, getDoc } from "firebase/firestore";
 
-export default function Page() {
-return <ComponentExample />;
+export default async function Page() {
+    const d = await getDoc(doc(collection(db, "example"), "c648kMelO39uJdVGq2CX"))
+    return <><ComponentExample /><div className="p-4">{d.data()?.name}</div></>;
 }
